fb23cb4211a9fc8b7787b07d70bec4765d07a557
Gunar-Callebaut <gunar.callebaut@student.hogent.be>
2023-03-02 09:36:14 +0100

    Update brusselsAirlines.py

--- brusselsAirlines.py
+++ brusselsAirlines.py
@@ -22,24 +22,27 @@ driver_service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=driver_service,options=options)
 driver.get(url)
 
-#klikken voor naar juiste pagina te gaan  
-r = driver.find_element(By.CSS_SELECTOR, "div.bp-mock p a.button")
+#klikken voor naar juiste pagina te gaan
+r = driver.find_element(By.CSS_SELECTOR, "button#searchFlights") # Findbutton by CSS selector
+# LAbel has the following class: checkbox-like lh lh-checkmark-checked
+
+label_click = driver.find_element(By.CSS_SELECTOR, "label.checkbox-like.lh.lh-checkmark-checked")
+print(label_click)
+driver.execute_script("arguments[0].click()", label_click) # Click the button
 driver.execute_script("arguments[0].click()", r)
 
 
-#wachten tot pagina laad
-time.sleep(40)
+#wachten tot pagina geladen is
+# s = driver.getCurrentUrl()
+# while(not s.equals(url)):
+#     #wachten tot pagina laad
+#     time.sleep(1)
+time.sleep(60)
 
-#DIT WERKT NOG NIET
-#klikken op de "enkele reis" knop en opnieuw laden
-#pelle testing things
-#l = driver.find_element(By.CSS_SELECTOR, "input.0")
-#l = driver.find_element_by_name("tripType")
-#l =driver.find_element(By.CLASS_NAME("input O ng-untouched ng-pristine ng-valid"));
 
-l = driver.find_element(By.XPATH(".//label[@class='label ng-star-inserted']/input[1][@class='input O ng-untouched ng-pristine ng-valid']"))
-
-#"input O ng-untouched ng-pristine ng-valid" 
-if(l): print("gelukt")
-#driver.execute_script("arguments[0].click()", l)
-
+#data ophalen
+reizen = driver.find_elements(By.CSS_SELECTOR, "cont-avail.ng-tns-c78-1")
+tel = 0
+for r in reizen:
+    tijden = r.find_element(By.CSS_SELECTOR, "span.time")
+    print(tijden.text)
